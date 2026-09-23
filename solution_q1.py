79aec441fecd816f367f2c7129e311a5feff7a9f
Misterhungery21 <sachingoindi@gmail.com>
2026-09-23 18:42:07 -0400

    Updated BFS and DFS

--- solution_q1.py
+++ solution_q1.py
@@ -1,13 +1,14 @@
 
+from collections import deque
 INITAL_MOVES = [(2,0), (1,1), (0,2), (1,0), (0,1)]
 
 p = []
 
 def read_state():
-    line = open("input.txt").readline().strip()
-    for x in line.split(","):
-        p.append(x.strip())
-
+    file=open("input.txt","r")
+    text=file.read()
+    file.close()
+    p=text.split(",")
     return (int(p[0]), int(p[1]), int(p[2]), int(p[3]), p[4].upper())
 
 def is_valid(s):
@@ -20,19 +21,53 @@ def is_valid(s):
         return False
     return True
 
-def successor(s):
-    m_left, c_left, m_right, c_right, boat = s
-    results = []
+def successor(state):
+    ml,cl,mr,cr,side=state
+    childern=[]
 
-    for m, c in INITAL_MOVES:
-        if boat == "L":
-            next = (m_left - m, c_left - c, m_right + m, c_right + c, "R")
-        else:
-            next = (m_left + m, c_left + c, m_right - m, c_right - c, "L")
+    if side == "L":
+        # 1 missionary
+        if is_valid((ml-1, cl, mr+1, cr, "R")):
+            childern.append((ml-1, cl, mr+1, cr, "R"))
 
-        if is_valid(next):
-            results.append(next)
-    return results
+        # 1 cannibal
+        if is_valid((ml, cl-1, mr, cr+1, "R")):
+            childern.append((ml, cl-1, mr, cr+1, "R"))
+
+        # 2 missionaries
+        if is_valid((ml-2, cl, mr+2, cr, "R")):
+            childern.append((ml-2, cl, mr+2, cr, "R"))
+
+        # 2 cannibals
+        if is_valid((ml, cl-2, mr, cr+2, "R")):
+            childern.append((ml, cl-2, mr, cr+2, "R"))
+
+        # 1 missionary and 1 cannibal
+        if is_valid((ml-1, cl-1, mr+1, cr+1, "R")):
+            childern.append((ml-1, cl-1, mr+1, cr+1, "R"))
+
+    if side == "R":
+        # 1 missionary
+        if is_valid((ml+1, cl, mr-1, cr, "L")):
+            childern.append((ml+1, cl, mr-1, cr, "L"))
+
+        # 1 cannibal
+        if is_valid((ml, cl+1, mr, cr-1, "L")):
+            childern.append((ml, cl+1, mr, cr-1, "L"))
+
+        # 2 missionaries
+        if is_valid((ml+2, cl, mr-2, cr, "L")):
+            childern.append((ml+2, cl, mr-2, cr, "L"))
+
+        # 2 cannibals
+        if is_valid((ml, cl+2, mr, cr-2, "L")):
+            childern.append((ml, cl+2, mr, cr-2, "L"))
+
+        # 1 missionary and 1 cannibal
+        if is_valid((ml+1, cl+1, mr-1, cr-1, "L")):
+            childern.append((ml+1, cl+1, mr-1, cr-1, "L"))
+
+    return childern
 
 def show(path):
     steps = []
@@ -69,3 +104,37 @@ print("The solution of 1.1.a (DFS) is:")
 print("Solution Path: ", show(path))
 print("Total cost =", len(path) - 1)
 print("Number of node expansions = ",expand)
+
+
+
+
+
+#BFS
+def BFS():
+    start=read_state()
+
+    #this variable keeps track of the expansions that happens
+    expansions=0
+
+
+    #I am going to format it as (state, cost, path to that node)
+    q = deque()
+    q.append((start, 0, [start]))
+    while q:
+        state, cost, path_list=q.popleft()
+        
+        if state==(0,0,3,3,"R"):
+            
+            print("The solution of Q1.1.b (BFS) is:")
+            print("Solution Path:", path_list)
+            print("Total cost =", cost)
+            print("Number of node expansions =", expansions)
+            break
+
+        expansions+=1
+
+        for node in get_valid_chidern(state):
+            q.append((node, cost+1, path_list+[node]))
+
+        
+  
